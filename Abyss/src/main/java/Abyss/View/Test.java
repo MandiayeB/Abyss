e76964092a318d736555544e3f7c6da2ee524fe9
MandiayeB <badiane@intechinfo.fr>
2020-10-12 11:56:52 +0200

    Test

---
+++ Abyss/src/main/java/Abyss/View/Test.java
@@ -0,0 +1,21 @@
+package Abyss.View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+
+	public static void main(String[] args) {
+
+			launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		primaryStage.show();
+		
+	}
+
+}
