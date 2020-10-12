c3e88b33db940cfc046166fe284c288231271407
MandiayeB <badiane@intechinfo.fr>
2020-10-12 12:35:46 +0200

    Test

--- Abyss/src/main/java/Abyss/View/Test.java
+++ Abyss/src/main/java/Abyss/View/Test.java
@@ -1,6 +1,12 @@
 package Abyss.View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Test extends Application {
@@ -13,7 +19,29 @@ public class Test extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		Button btn = new Button("Click me");
+		Button exit = new Button("Exit");
+		exit.setOnAction(e -> System.exit(0));
+		exit.setOnAction(e -> {
+			System.out.println("Bye!");
+			System.exit(0);
+		
+		});
 
+		btn.setOnAction(new EventHandler<ActionEvent>(	) {
+			
+			public void handle(ActionEvent event) {
+				
+				System.out.println("Hello World");
+				
+			}
+		});
+		VBox root = new VBox();
+		root.getChildren().addAll(btn, exit);
+		Scene scene = new Scene(root,500,300);
+		primaryStage.setTitle("My title");
+		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
