dd7ac3257f0af3b0188e14273c0848ca0638f77a
allan <allan@Legion-Takoo>
2020-11-05 13:03:53 +0100

    Prototype du Gacha

--- Abyss/src/main/java/org/abyss/javafxview/Main.java
+++ Abyss/src/main/java/org/abyss/javafxview/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
 			FXMLLoader loaderAccueil = new FXMLLoader(getClass().getResource("/resources/FXML/Accueil.fxml"));
 			FXMLLoader loaderMain = new FXMLLoader(getClass().getResource("/resources/FXML/FXML.fxml"));
 			FXMLLoader loaderCollection = new FXMLLoader(getClass().getResource("/resources/FXML/Collection.fxml"));
+			FXMLLoader loaderGacha = new FXMLLoader(getClass().getResource("/resources/FXML/Gacha.fxml"));
 			
 			Parent accueil = loaderAccueil.load();
 			Scene sceneAccueil = new Scene(accueil);
@@ -39,11 +40,15 @@ public class Main extends Application {
 			Parent game = loaderMain.load();
 			Scene sceneGame = new Scene(game);
 			
+			Parent gacha = loaderGacha.load();
+			Scene sceneGacha = new Scene(gacha);
+			
 			
 			//CSS
 			accueil.getStylesheets().add(getClass().getResource("/resources/CSS/Accueil.css").toExternalForm());
 			sceneGame.getStylesheets().add(getClass().getResource("/resources/CSS/style.css").toExternalForm());
 			sceneCollection.getStylesheets().add(getClass().getResource("/resources/CSS/Collection.css").toExternalForm());
+			sceneGacha.getStylesheets().add(getClass().getResource("/resources/CSS/Collection.css").toExternalForm());
 			
 			//On fournit au controlleur la scene jeu
 			//Bouton jouer pour aller a la scene de jeu
@@ -65,6 +70,11 @@ public class Main extends Application {
 			MainController mc = (MainController) loaderMain.getController();
 			mc.setScene(sceneAccueil);
 			mc.setStage(primaryStage);
+			
+			//Gacha 
+			CollectionController collec = (CollectionController) loaderCollection.getController();
+			collec.setSceneGacha(sceneGacha);
+			collec.setStage(primaryStage);
 			
 			//On change le curseur 
 			Image image = new Image("/resources/Images/curseur.png");
