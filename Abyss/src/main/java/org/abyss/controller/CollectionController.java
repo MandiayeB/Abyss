78d49ada79b8effa06fc01f001996dd9d7bf2330
allan <allan@Legion-Takoo>
2020-11-09 15:42:17 +0100

    Gacha 1.0

--- Abyss/src/main/java/org/abyss/controller/CollectionController.java
+++ Abyss/src/main/java/org/abyss/controller/CollectionController.java
@@ -14,9 +14,25 @@ import javafx.stage.Stage;
 public class CollectionController implements Initializable  {
 	
 	@FXML
-	private Label rien1;
-	@FXML
 	private ImageView drop1;
+	@FXML
+	private ImageView carte1;
+	@FXML
+	private ImageView carte2;
+	@FXML
+	private ImageView carte3;
+	@FXML
+	private ImageView carte4;
+	@FXML
+	private ImageView carte5;
+	@FXML
+	private ImageView carte6;
+	@FXML
+	private ImageView carte7;
+	@FXML
+	private ImageView carte8;
+	@FXML
+	private ImageView carte9;
 
 	Stage stage;
 	Scene scene;
@@ -54,18 +70,34 @@ public class CollectionController implements Initializable  {
 	public void retour() {
 		stage.setScene(scene);
 	}
-	public void gacha() {
+	public void gacha1() {
 		stage.setScene(sceneGacha);
 	}
 	public void invoc1() {
-		int random = (int) (1 + (Math.random()* 100)); // Random va de 1 a 50
+		int random = (int) ((Math.random()* 50)); // Random va de 1 a 50
 		System.out.println(random);
-		if (random > 10) {
-			rien1.setText("Tu n'as rien eu ");
+		if (random < 10) {
+			drop1.setImage(new Image("/resources/Images/dos.jpg"));
 		}
-		else {
+		if (random > 10 && random < 20) {
 			drop1.setImage(new Image("/resources/Images/yugi.jpg"));
-			rien1.setText("");
+			carte1.setImage(new Image("/resources/Images/yugi.jpg"));
+		}
+		if (random > 20 && random < 30) {
+			drop1.setImage(new Image("/resources/Images/yugi1.jpg"));
+			carte2.setImage(new Image("/resources/Images/yugi1.jpg"));
+		}
+		if (random > 30 && random < 40) {
+			drop1.setImage(new Image("/resources/Images/yugi2.jpg"));
+			carte3.setImage(new Image("/resources/Images/yugi2.jpg"));
+		}
+		if (random > 40 && random < 45) {
+			drop1.setImage(new Image("/resources/Images/yugi3.jpg"));
+			carte4.setImage(new Image("/resources/Images/yugi3.jpg"));
+		}
+		if (random > 45 && random < 50) {
+			drop1.setImage(new Image("/resources/Images/yugi4.jpg"));
+			carte5.setImage(new Image("/resources/Images/yugi4.jpg"));
 		}
 	}
 }
